ee7f0d91200a0107cf0c0dda951a318960b7a62e
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-07-10 22:44:02 -0400

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('dotenv').config()
 const express = require('express')
+require('dotenv').config()
 const app = express()
 
 app.use('/places', require('./controllers/places'))
@@ -12,7 +12,8 @@ app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
+const PORT = process.env.PORT
 
-app.listen(process.env.PORT)
+app.listen(PORT, console.log(`listening on port ${PORT}`))
 
 
