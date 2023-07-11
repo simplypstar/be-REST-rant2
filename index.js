6d721ec70fbec8f666423370d48d3394d8c7e4e2
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-07-10 21:52:28 -0400

    Initial commit

--- index.js
+++ index.js
@@ -14,16 +14,3 @@ app.get('*', (req, res) => {
 app.listen(process.env.PORT)
 
 
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
-
-app.listen(process.env.PORT)
